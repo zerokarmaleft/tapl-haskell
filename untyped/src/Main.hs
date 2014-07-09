ec8067c50d8891a8fd2c3b0a1d077b6dc65a6ae8
Edward Cho <zerokarmaleft@gmail.com>
2014-07-09 15:56:58 -0500

    show parse tree instead of pretty printed terms until deBruijn indices get fixed

--- untyped/src/Main.hs
+++ untyped/src/Main.hs
@@ -5,16 +5,16 @@ import Text.Parsec
 import Untyped.Context
 import Untyped.Evaluator
 import Untyped.Parser
+import Untyped.Syntax
 
 main :: IO ()
 main =
   do args <- getArgs
      case args of
        [sourceFile] ->
-         do parseTree <- fmap (runParser parseTerm mkContext "untypedi")
-                             (readFile sourceFile)
-            putStrLn $ show parseTree
+         do let ctx = mkContext
+            parseTree <- fmap (runParser parseTerm ctx "untyped") (readFile sourceFile)
             case parseTree of
-              Right expr -> putStrLn $ "=> " ++ (show . eval) expr
+              Right expr -> putStrLn $ (show parseTree) ++ "\n=> " ++ (show . eval) expr
               Left err   -> putStrLn $ show err
-       _ -> putStrLn "Usage: untypedi <sourceFile>"
+       _ -> putStrLn "Usage: untyped <sourceFile>"
