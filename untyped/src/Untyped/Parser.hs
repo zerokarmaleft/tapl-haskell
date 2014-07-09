c206237f8e38ea2bfb59779d67e9b57c0faf8c9a
Edward Cho <zerokarmaleft@gmail.com>
2014-07-09 15:57:47 -0500

    add type sigs

--- untyped/src/Untyped/Parser.hs
+++ untyped/src/Untyped/Parser.hs
@@ -2,11 +2,14 @@
 
 module Untyped.Parser where
 
+import           Data.Functor.Identity
 import           Data.List
 import           Text.Parsec
 import qualified Text.Parsec.Token as P
 import           Untyped.Context
 import           Untyped.Syntax
+
+type Parser a = ParsecT String Context Identity a
 
 untypedDef :: P.LanguageDef st
 untypedDef =
@@ -23,24 +26,35 @@ untypedDef =
                 , P.caseSensitive  = True
                 }
 
+lexer :: P.GenTokenParser String u Identity
 lexer = P.makeTokenParser untypedDef
 
+dot :: ParsecT String u Identity String
 dot        = P.dot        lexer
+
+identifier :: ParsecT String u Identity String
 identifier = P.identifier lexer
+
+parens :: ParsecT String u Identity a -> ParsecT String u Identity a
 parens     = P.parens     lexer
+
+reserved :: String -> ParsecT String u Identity ()
 reserved   = P.reserved   lexer
 
+getVarIndex :: (Monad m, Eq a) => a -> [a] -> m Int
 getVarIndex var ctx =
   case elemIndex var ctx of
     Just i  -> return i
     Nothing -> error "Unbound variable name"
 
+parseVar :: Parser Term
 parseVar =
   do var <- identifier
      ctx <- getState
      idx <- getVarIndex var ctx
      return $ TermVar idx (length ctx)
 
+parseAbs :: Parser Term
 parseAbs =
   do reserved "lambda"
      var  <- identifier
@@ -51,8 +65,7 @@ parseAbs =
      setState ctx
      return $ TermAbs var term
 
-parseNonApp = parseAbs <|> parseVar <|> parens parseTerm
-
+parseTerm :: Parser Term
 parseTerm = 
   chainl1 (parseAbs <|> parseVar <|> parens parseTerm) 
           (return TermApp) 
