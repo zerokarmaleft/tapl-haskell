234ea2c8ebe957ee166478c1e8d89c929b4f943f
Edward Cho <zerokarmaleft@gmail.com>
2014-07-10 14:15:22 -0500

    remove trace calls

--- untyped/src/Untyped/Evaluator.hs
+++ untyped/src/Untyped/Evaluator.hs
@@ -1,39 +1,26 @@
 module Untyped.Evaluator where
 
 import Control.Monad
-import Debug.Trace
 import Untyped.Syntax
 
 -- Variable Shifting and Substitution
 --
 
 shiftTerm :: Int -> Term -> Term
--- shiftTerm d t = walk 0 t
---   where walk c (TermVar x n)
---           | n >= c             = TermVar (x+d) (n+d)
---           | otherwise          = TermVar x     (n+d)
---         walk c (TermAbs x t1)  = TermAbs x (walk (c+1) t1)
---         walk c (TermApp t1 t2) = TermApp (walk c t1) (walk c t2)
-shiftTerm d t = trace ("shiftTerm[" ++ show d ++ "]: \t\t" ++ show t) $ walk 0 t
+shiftTerm d = walk 0
   where walk c (TermVar x n)
-          | x >= c             = trace ("shiftWalk[" ++ show (c,d) ++ "]: \t" ++ show (TermVar x n)) $ TermVar (x+d) (n+d)
-          | otherwise          = trace ("shiftWalk[" ++ show (c,d) ++ "]: \t" ++ show (TermVar x n)) $ TermVar x     (n+d)
-        walk c (TermAbs x t1)  = trace ("shiftWalk[" ++ show (c,d) ++ "]: \t" ++ show (TermAbs x t1)) $ TermAbs x (walk (c+1) t1)
-        walk c (TermApp t1 t2) = trace ("shiftWalk[" ++ show (c,d) ++ "]: \t" ++ show (TermApp t1 t2)) $ TermApp (walk c t1) (walk c t2)
+          | x >= c             = TermVar (x+d) (n+d)
+          | otherwise          = TermVar x     (n+d)
+        walk c (TermAbs x t1)  = TermAbs x (walk (c+1) t1)
+        walk c (TermApp t1 t2) = TermApp (walk c t1) (walk c t2) 
 
 substTerm :: Int -> Term -> Term -> Term
--- substTerm j s t = walk 1 t
---   where walk c (TermVar x n)
---           | n == j+c           = shiftTerm c s
---           | otherwise          = TermVar x n
---         walk c (TermAbs x t1)  = TermAbs x (walk (c+1) t1)
---         walk c (TermApp t1 t2) = TermApp (walk c t1) (walk c t2)
-substTerm j s t = trace ("substTerm[" ++ show j ++ "]: \t\t" ++ show (s,t)) $ walk 0 t
+substTerm j s = walk 0
   where walk c (TermVar x n)
-          | x == j+c           = trace ("substWalk[" ++ show (c,j) ++ "]: \t" ++ show (TermVar x n)) $ s
-          | otherwise          = trace ("substWalk[" ++ show (c,j) ++ "]: \t" ++ show (TermVar x n)) $ TermVar x n
-        walk c (TermAbs x t1)  = trace ("substWalk[" ++ show (c,j) ++ "]: \t" ++ show (TermAbs x t1)) $ TermAbs x (walk (c+1) t1)
-        walk c (TermApp t1 t2) = trace ("substWalk[" ++ show (c,j) ++ "]: \t" ++ show (TermApp t1 t2)) $ TermApp (walk c t1) (walk c t2)
+          | x == j+c           = s
+          | otherwise          = TermVar x n
+        walk c (TermAbs x t1)  = TermAbs x (walk (c+1) t1)
+        walk c (TermApp t1 t2) = TermApp (walk c t1) (walk c t2)
 
 substTopTerm :: Term -> Term -> Term
 substTopTerm s t = shiftTerm (-1) (substTerm 0 (shiftTerm 1 s) t)
