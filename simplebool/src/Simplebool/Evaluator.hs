b49a332174f4371e548ac8cad4a60e0f8659c08b
Edward Cho <zerokarmaleft@gmail.com>
2014-08-03 16:25:25 -0500

    implement parser and evaluator

--- simplebool/src/Simplebool/Evaluator.hs
+++ simplebool/src/Simplebool/Evaluator.hs
@@ -1,3 +1,59 @@
 module Simplebool.Evaluator where
 
-eval = undefined
+import Control.Monad
+import Simplebool.Syntax
+
+-- Variable Shifting and Substitution
+--
+
+shiftTerm :: Int -> Term -> Term
+shiftTerm d = walk 0
+  where walk c (TermIf t1 t2 t3)   = TermIf (walk c t1) (walk c t2) (walk c t3)
+        walk c (TermVar x n)
+          | x >= c                 = TermVar (x+d) (n+d)
+          | otherwise              = TermVar x     (n+d)
+        walk c (TermAbs x tyT1 t1) = TermAbs x tyT1 (walk (c+1) t1)
+        walk c (TermApp t1 t2)     = TermApp (walk c t1) (walk c t2)
+        walk _ t                   = t
+
+substTerm :: Int -> Term -> Term -> Term
+substTerm j s = walk 0
+  where walk c (TermIf t1 t2 t3)   = TermIf (walk c t1) (walk c t2) (walk c t3)
+        walk c (TermVar x n)
+          | x == j+c               = s
+          | otherwise              = TermVar x n
+        walk c (TermAbs x tyT1 t1) = TermAbs x tyT1 (walk (c+1) t1)
+        walk c (TermApp t1 t2)     = TermApp (walk c t1) (walk c t2)
+        walk _ t1
+          | t1 == TermTrue         = t1
+          | t1 == TermFalse        = t1
+          | otherwise              = s
+
+applyAbs :: Term -> Term -> Term
+applyAbs s t = shiftTerm (-1) (substTerm 0 (shiftTerm 1 s) t)
+
+-- Evaluation
+--
+
+isValue :: Term -> Bool
+isValue (TermAbs _ _ _) = True
+isValue TermTrue        = True
+isValue TermFalse       = True
+isValue _               = False
+
+eval1 :: Term -> Maybe Term
+eval1 (TermIf TermTrue  t2 _ ) = return t2
+eval1 (TermIf TermFalse _  t3) = return t3
+eval1 (TermIf t1        t2 t3) = liftM (\t1' -> TermIf t1' t2 t3) (eval1 t1)
+eval1 (TermApp (TermAbs _ _ t12) v2)
+  | isValue v2                 = return $ applyAbs v2 t12
+eval1 (TermApp t1 t2)
+  | isValue t1                 = liftM2 TermApp (return t1) (eval1  t2)
+  | otherwise                  = liftM2 TermApp (eval1  t1) (return t2)
+eval1 _                        = Nothing
+
+eval :: Term -> Term
+eval t =
+  case eval1 t of
+    Just t' -> eval t'
+    Nothing -> t
--- simplebool/src/Simplebool/Parser.hs
+++ simplebool/src/Simplebool/Parser.hs
@@ -1,3 +1,132 @@
 module Simplebool.Parser where
 
-parseTerm = undefined
+import           Data.Functor.Identity
+import           Data.List
+import           Simplebool.Context
+import           Simplebool.Syntax
+import           Text.Parsec
+import qualified Text.Parsec.Token as P
+
+type Parser a = ParsecT String Context Identity a
+
+simpleboolDef :: P.LanguageDef st
+simpleboolDef =
+  P.LanguageDef { P.commentStart    = ""
+                , P.commentEnd      = ""
+                , P.commentLine     = ""
+                , P.nestedComments  = True
+                , P.identStart      = letter
+                , P.identLetter     = alphaNum
+                , P.opStart         = letter
+                , P.opLetter        = alphaNum
+                , P.reservedOpNames = [ "lambda"
+                                      , "if"
+                                      , "then"
+                                      , "else"
+                                      , "true"
+                                      , "false"
+                                      , "Bool"
+                                      ]
+                , P.reservedNames   = [ "lambda"
+                                      , "if"
+                                      , "then"
+                                      , "else"
+                                      , "true"
+                                      , "false"
+                                      , "Bool"
+                                      ]
+                , P.caseSensitive   = True
+                }
+
+lexer :: P.GenTokenParser String u Identity
+lexer = P.makeTokenParser simpleboolDef
+
+colon :: ParsecT String u Identity String
+colon = P.colon lexer
+
+dot :: ParsecT String u Identity String
+dot        = P.dot        lexer
+
+identifier :: ParsecT String u Identity String
+identifier = P.identifier lexer
+
+parens :: ParsecT String u Identity a -> ParsecT String u Identity a
+parens     = P.parens     lexer
+
+reservedOp :: String -> ParsecT String u Identity ()
+reservedOp = P.reservedOp lexer
+
+reserved :: String -> ParsecT String u Identity ()
+reserved   = P.reserved   lexer
+
+parseTrue :: Parser Term
+parseTrue  = reserved "true"  >> return TermTrue
+
+parseFalse :: Parser Term
+parseFalse = reserved "false" >> return TermFalse
+
+parseIf :: Parser Term
+parseIf =
+  do reservedOp "if"
+     predicate  <- parseTerm
+     reservedOp "then"
+     consequent <- parseTerm
+     reservedOp "else"
+     antecedent <- parseTerm
+     return $ TermIf predicate consequent antecedent
+
+getVarIndex :: (Monad m, Eq a) => a -> [(a,b)] -> m Int
+getVarIndex var ctx =
+  case findIndex ((== var) . fst) ctx of
+    Just i  -> return i
+    Nothing -> error "Unbound variable name"
+
+parseVar :: Parser Term
+parseVar =
+  do var <- identifier
+     ctx <- getState
+     idx <- getVarIndex var ctx
+     return $ TermVar idx (length ctx)
+
+parseAbs :: Parser Term
+parseAbs =
+  do reservedOp "lambda"
+     var   <- identifier
+     tyVar <- parseTypeAnnotation
+     dot
+     ctx   <- getState
+     setState $ addBinding (var, VarBinding tyVar) ctx
+     term  <- parseTerm
+     setState ctx
+     return $ TermAbs var tyVar term
+
+parseTypeBool :: Parser Type
+parseTypeBool = reserved "Bool" >> return TypeBool
+
+parseTypeArrow :: Parser Type
+parseTypeArrow =
+  do tyT1 <- parseTypeBool
+     many space
+     string "->"
+     many space
+     tyT2 <- parseType
+     return $ TypeArrow tyT1 tyT2
+
+parseType :: Parser Type
+parseType =
+  try parseTypeArrow <|> parseTypeBool <|> parens parseType
+
+parseTypeAnnotation :: Parser Type
+parseTypeAnnotation =
+  do colon
+     parseType
+
+parseTerm :: Parser Term
+parseTerm =
+  chainl1 (parseTrue  <|>
+           parseFalse <|>
+           parseIf    <|>
+           parseAbs   <|>
+           parseVar   <|>
+           parens parseTerm)
+          (return TermApp)
