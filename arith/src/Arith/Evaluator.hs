ca2d00d07fc6b823e1d1c6fa3ca741dc6a101b19
Edward Cho <zerokarmaleft@gmail.com>
2014-08-04 14:33:45 -0500

    lift eval1 of child terms into Maybe

--- arith/src/Arith/Evaluator.hs
+++ arith/src/Arith/Evaluator.hs
@@ -1,35 +1,22 @@
 module Arith.Evaluator (eval1, eval) where
 
 import Arith.Syntax
+import Control.Monad
 
 eval1 :: Term -> Maybe Term
-eval1 TermTrue                 = Nothing
-eval1 TermFalse                = Nothing
-eval1 (TermIf TermTrue  t2 _ ) = Just t2
-eval1 (TermIf TermFalse _  t3) = Just t3
-eval1 (TermIf p         t2 t3) = 
-  case eval1 p of
-    Just TermTrue  -> Just $ TermIf TermTrue t2 t3
-    Just TermFalse -> Just $ TermIf TermFalse t2 t3
-    Just t1        -> Just $ TermIf t1 t2 t3
-    Nothing        -> Nothing
-eval1 TermZero                 = Nothing
-eval1 (TermSucc t)             = 
-  case eval1 t of
-    Just t' -> Just $ TermSucc t'
-    Nothing -> Nothing 
-eval1 (TermPred TermZero)      = Just TermZero
-eval1 (TermPred (TermSucc t))  = Just t
-eval1 (TermPred t)             = 
-  case eval1 t of
-    Just t' -> Just $ TermPred t'
-    Nothing -> Nothing
-eval1 (TermIsZero TermZero)    = Just TermTrue
+eval1 TermTrue                         = Nothing
+eval1 TermFalse                        = Nothing
+eval1 (TermIf TermTrue  t2 _ )         = Just t2
+eval1 (TermIf TermFalse _  t3)         = Just t3
+eval1 (TermIf t1        t2 t3)         = liftM3 TermIf (eval1 t1) (return t2) (return t3)
+eval1 TermZero                         = Nothing
+eval1 (TermSucc t)                     = liftM TermSucc (eval1 t)
+eval1 (TermPred TermZero)              = Just TermZero
+eval1 (TermPred (TermSucc t))          = Just t
+eval1 (TermPred t)                     = liftM TermPred (eval1 t)
+eval1 (TermIsZero TermZero)            = Just TermTrue
 eval1 (TermIsZero (TermSucc TermZero)) = Just TermFalse
-eval1 (TermIsZero t)           = 
-  case eval1 t of
-    Just t' -> Just $ TermIsZero t'
-    Nothing -> Nothing
+eval1 (TermIsZero t)                   = liftM TermIsZero (eval1 t)
 
 eval :: Term -> Term
 eval t =
